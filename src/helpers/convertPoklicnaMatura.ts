fdd6222906bb9300d4d8816a3a3726df5e8669ec
primozpadar <primoz.padar1@gmail.com>
2020-07-27 20:58:45 +0200

    Refactored - pretvorba točk

--- src/helpers/convertPoklicnaMatura.ts
+++ src/helpers/convertPoklicnaMatura.ts
@@ -10,64 +10,13 @@ const convert23 = (val: number) => {
       return 95;
     case 19:
       return 93;
-    case 18:
-      return 90;
-    case 17:
-      return 85;
-    case 16:
-      return 80;
-    case 15:
-      return 75;
-    case 14:
-      return 70;
-    case 13:
-      return 65;
-    case 12:
-      return 60;
-    case 11:
-      return 50;
-    case 10:
-      return 45.7;
-    case 9:
-      return 45;
-    case 8:
-      return 40;
     default:
-      throw Error;
+      return val * 5;
   }
 };
 
 const convert20 = (val: number) => {
-  switch (val) {
-    case 20:
-      return 100;
-    case 19:
-      return 95;
-    case 18:
-      return 90;
-    case 17:
-      return 85;
-    case 16:
-      return 80;
-    case 15:
-      return 75;
-    case 14:
-      return 70;
-    case 13:
-      return 65;
-    case 12:
-      return 60;
-    case 11:
-      return 50;
-    case 10:
-      return 45.7;
-    case 9:
-      return 45;
-    case 8:
-      return 40;
-    default:
-      throw Error;
-  }
+  return val * 5;
 };
 
 export { convert23, convert20 };
